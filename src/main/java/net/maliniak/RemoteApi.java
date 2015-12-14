c63ce32e50492e575615b80367458590e733ed20
L.Malinowski <maliniakh@gmail.com>
2015-12-14 23:31:16 +0000

    removed remoteExceptions

--- src/main/java/net/maliniak/RemoteApi.java
+++ src/main/java/net/maliniak/RemoteApi.java
@@ -3,18 +3,17 @@ package net.maliniak;
 import net.maliniak.model.Site;
 import net.maliniak.model.Window;
 
-import java.rmi.RemoteException;
 import java.util.List;
 
 /**
  * Created by llmali on 01/12/2015.
  */
 public interface RemoteApi {
-    Window getUniqueWindow(Site site) throws RemoteException;
+    Window getUniqueWindow(Site site);
 
-    Window getUniqueWindow(String titleRegex) throws RemoteException;
+    Window getUniqueWindow(String titleRegex);
 
-    List<Window> getWindows(Integer processId, String titleRegex) throws RemoteException;
+    List<Window> getWindows(Integer processId, String titleRegex);
 
 //    List<Window> getChildWindows(WinDef.HWND hwnd, String titleRegex) throws RemoteException;
 }
